6b8d3c9116ecbc430bf708be73184a35fe117de8
matrixmike <matroxmike@icloud.com>
2025-03-16 10:06:29 +1100

    early commit 4

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,9 +5,46 @@ extern "C" JNIEXPORT jstring
 Java_com_example_mynative1_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
+
+    class Shape {
+    protected:
+        int width, height;
+
+    public:
+        explicit Shape( int a = 0, int b = 0) {
+            width = a;
+            height = b;
+        }
+
+        virtual int area() {
+  //          cout << "Parent class area :" <<endl;
+            return 0;
+        }
+    };
+
+    class Rectangle: public Shape {
+    public:
+        explicit Rectangle( int a = 0, int b = 0):Shape(a, b) { }
+        int area () override {
+   //         cout << "Rectangle class area :" <<endl;
+            return (width * height);
+        }
+    };
+
+    class Triangle: public Shape{
+    public:
+        explicit Triangle( int a = 0, int b = 0):Shape(a, b) { }
+        int area () override {
+     //       cout << "Triangle class area :" <<endl;
+            return (width * height / 2);
+        }
+    };
+    char buffer [sizeof(unsigned int)*8+1];
+
     int ledPins[] = {
             2, 7, 4, 6, 5, 3
     };                 // an array of pin numbers to which LEDs are attached
-    std::string hello = "Hello from C++  B" ;
+    std::string hello = "Hello from C++  E" ;
+    // hello = itoa(99, buffer, DECIMAL_DIG)  ;
     return env->NewStringUTF(hello.c_str());
 }
